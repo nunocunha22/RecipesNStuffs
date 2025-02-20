ebe8d134614863c327ebd8e5450725054c67c74b
Your Name <nunocunha22@gmail.com>
2025-02-20 18:17:52 +0000

    Homepage updated. New pictures added. new media queries added for homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -3,8 +3,20 @@ import React from "react";
 export default function Homepage() {
     return (
         <div className="margin-top-32 homepagePicture">
-            <p className="homepageText1">Estamos a trabalhar arduamente para construir esta página</p>
-            <p className="homepageText2">As outas páginas estão disponiveis</p>
+
+            <h1 className="homepageIntro">Cozinhar é uma jornada de descobertas, e nem sempre o caminho é direto.</h1>
+
+            <p className="homepageText1">Por trás de cada receita bem-sucedida, houve muitas tentativas, erros inesperados e ajustes cuidadosos. Já aconteceu o bolo não crescer, o tempero ficar exagerado, a massa não ter a textura certa… e está tudo bem! Cozinhar é um processo de aprendizagem e experimentação, onde cada erro nos ensina algo novo.
+
+                Neste espaço, partilho receitas testadas (e retestadas!) para que tenha a melhor experiência possível na sua cozinha. Mas lembre-se: não há problema se não sair perfeito à primeira! O importante é tentar, ajustar e, acima de tudo, desfrutar do prazer de cozinhar. Afinal, é nos pequenos desafios que se esconde a verdadeira magia da culinária.
+
+                Foram muitas as tentativas, os erros, os ajustes e as descobertas até que cada receita chegasse ao ponto certo. E está tudo bem! Nem sempre sai perfeito à primeira, e é isso que torna a cozinha tão especial. Aqui, partilho receitas testadas (e retestadas!) para que cada prato seja uma experiência deliciosa.
+            </p>
+            <p className="homepageText2">
+                <p>Então, mãos à massa e aproveite cada momento!</p>
+                <p>Experimente, adapte e, acima de tudo, divirta-se....!</p>
+            </p>
+
         </div>
 
     )
