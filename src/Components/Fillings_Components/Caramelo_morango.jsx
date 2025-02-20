6b214a116632fa455786d58dfc3688324fd41642
Your Name <nunocunha22@gmail.com>
2025-02-20 18:41:42 +0000

    Caramelo_morango updated

--- src/Components/Fillings_Components/Caramelo_morango.jsx
+++ src/Components/Fillings_Components/Caramelo_morango.jsx
@@ -11,24 +11,28 @@ export default function CarameloMorango() {
                     <h3>&#45;&#62; para aproximadamente 50 a 60 macarrons</h3>
                     <h3>&#45;&#62; para aproximadamente 1 camada de bolo</h3>
                 </li>
-                <li>1 pacote de natas para bater (200ml)</li>
-                <li>150g de chocolate branco</li>
-                <li>50g de manteiga sem sal</li>
-                <li>100ml de sumo de limão</li>
-                <li>corante amarelo a gosto (opcional)</li>
-                <li>6g de agar-agar ou 20gr de gelatina sem sabor em pó (opcional! Só uso agar-agar ou gelatina quando é para rechear macarrons. Se for para recheio de bolo não uso! )</li>
+                <li>200gr de morango (congelados ou frescos)</li>
+                <li>180gr de açucar</li>
+                <li>&#189; pacote de natas para bater (100ml)</li>
+                <li>70g de manteiga sem sal</li>
+                <li>30gr de chocolate branco</li>
+                <li>5g de agar-agar ou 15gr de gelatina sem sabor em pó (opcional! Só uso agar-agar ou gelatina quando é para rechear macarrons. Se for para recheio de bolo não uso! )</li>
+                <li>Ácido cítrico (opcional)</li>
             </ul>
             <ul>
                 <li>
                     <h2>Preparação</h2>
                 </li>
                 <ol>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>Triturar os morangos e coar para que fique um sumo sem pedaços. Reservar</li>
+                    <li>Numa panela levar o açúcar para caramelizar.</li>
+                    <li>Quando já estiver praticamente todo derretido, colocar a manteiga e as natas para aquecer ligeiramente. Verter sobre o caramelo. Cuidado com os salpicos!! Mexer bem até incorporar tudo muito bem</li>
+                    <li>Verter o sumo de morango e misture bem para não ficar em grumos</li>
+                    <li>Acrescentar o chocolate branco e o ácido cítrico</li>
+                    <li>Acrescentar o agar-agar e deixar ferver uns 2min, mexendo de vez em quando.</li>
+                    <li>Filmar e colocar no frigorifico por umas 8h</li>
+                    <li>Quando for usar para rechear macarrons, aquecer em banho-maria e usar</li>
+                    <li>Para uma textura mais cremosa para rechear bolos também se pode bater a mistura. Atenção que o caramelo irá ficar com uma cor mais clara</li>
                 </ol>
             </ul>
             <ul>
